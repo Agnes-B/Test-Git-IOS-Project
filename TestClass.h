3fb49df0bc90aefe78abc6ba5e3c05737e5ef85e
Agnes Bolton <boltonagnes@gmail.com>
2015-04-04 16:15:13 +1100

    added a test class

---
+++ TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Git Test
+//
+//  Created by Agnes Bolton on 4/04/2015.
+//  Copyright (c) 2015 Agnes Bolton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Git Test
+//
+//  Created by Agnes Bolton on 4/04/2015.
+//  Copyright (c) 2015 Agnes Bolton. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
